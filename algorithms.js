06f02df2ef10fa63dba834f87fbec9e61cef925d
andrewnaeve <andrewnaeve@gmail.com>
2017-02-19 11:52:37 -0800

    algo correction

--- algorithms.js
+++ algorithms.js
@@ -12,34 +12,31 @@ module.exports.longestPhrase = function(twee) {
       if (companyString.includes(tweet[i]) && tweet[i][0] === tweet[i][0].toUpperCase()) {
         var t = tweet[i];
         consec.push(t);
-        if(tweet[i+1] !== undefined) {
-          for (var j=i+1; j < tweet.length; j++) {
+
+          for (var j=i+1; j < tweet.length-1; j++) {
             if (companyString.includes(t + ' ' + tweet[j])) {
               t += ' ' + tweet[j];
               consec.push(t);
-              console.log('in loop', t)
             } else {
               break;
             };
           };
-        }; 
+
       };
     };
   };
   // console.log(consec)
   if (consec.length > 0) {return(consec.reduce((a,b) => a.length > b.length ? a : b))}
-  else {return 'no matches from longest phrase'}
+  else { return 'no matches from longest phrase' };
 };
-// bug = algo checks one after, but fails if one after doesn't exist
 
  module.exports.findSymbol = function(longest) {
+
   let wordCount = longest.split(' ').length;
   let narrow = sorted.filter(x => x["Description"].includes(longest) && wordCount / x["Description"].split(' ').length > .3)
   .sort((a,b) => (longest.length / b["Description"].length) - (longest.length / a["Description"].length))
   
-  // sorting makes it more relevant, but if multiple matches, do you really wanna?
   return narrow
-  // console.log(count)
 
-}
+};
   
