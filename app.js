4cccc379f2cb5a42ebc8625e1d084ac41de98165
andrewnaeve <andrewnaeve@gmail.com>
2017-02-25 12:02:08 -0800

    etrade functions work

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const request = require('request');
 const logger = require('morgan');
 const twitter = require('twitter');
 const client = require('./secrets/twitter_keys').twit;
+const et = require('./secrets/etrade').et;
+const {key, secret} = require('./secrets/etrade').et;
 const sentiment = require('sentiment');
+
 
 // stock lists
 const nasdaq = require('./ticker_lists/nasdaq').nasdaq;
@@ -35,10 +39,23 @@ app.listen(port, function() {
 let params = {with: 'followings'};
 let stream = client.stream('user', params );
 
-stream.on('data', function(event) {
-  let tweet = event.text;
-  console.log('the tweet: ', tweet);
-  isTraded(tweet);
-})
+// stream.on('data', function(event) {
+//   let tweet = event.text;
+//   console.log('the tweet: ', tweet);
+//   isTraded(tweet);
+// })
 
-// isTraded("The FAKE NEWS media (failing @nytimes, @NBCNews, @ABC, @CBS, @CNN) is not my enemy, it is the enemy of the American People!".trim().replace(/[.,\/#!@$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," "))
+// et.getRequestToken(
+//   function(authorizationUrl) {
+//     console.log(authorizationUrl)
+//   }, function(error) {
+//     console.log('error ', error)
+//   }
+// );
+
+// et.getAccessToken('J7CXG',
+// function() {
+
+// }, function(error) {
+//   console.log(error)
+// });
